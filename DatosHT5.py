ec6b8df7c21213cf5d33199b9e4e7b1836c110da
Andy5am <38666859+Andy5am@users.noreply.github.com>
2019-02-27 17:54:48 -0600

    Simulacion hecha sin waiting

--- DatosHT5.py
+++ DatosHT5.py
@@ -5,11 +5,12 @@
 
 import simpy
 import random
+random.seed(5)
 
 class System:
     def __init__(self, env, instructionsRead):
         self.env = env
-        self.ram = simpy.Container(env, init= 100, capacity= 100)
+        self.ram = simpy.Container(env, init= 15, capacity= 15)
         self.cpu = simpy.Resource(env, capacity = 1)
         self.instructionsRead = instructionsRead
 
@@ -21,18 +22,26 @@ class Process:
         self.memory = memory
         self.instructions = instructions
         self.system = system
+        self.cronometro = 0
         self.action = env.process(self.ready(env,system))
 
     def ready(self,env,system):
-        if system.ram.level >= self.memory:
-            yield system.ram.get(self.memory)
-            print("Memoria proceso",self.name,"es",self.memory)
-            print("Queda",self.system.ram.level,"de RAM")
-            print("El proceso",self.name,"tiene",self.instructions,"instrucciones")
+        yield self.system.ram.get(self.memory)
+        print("ram",self.system.ram.level)
+        print("Memoria proceso",self.name,"es",self.memory)
+        print("El proceso",self.name,"tiene",self.instructions,"instrucciones\n")
+        while self.instructions > 0:
             with system.cpu.request() as req:
                 yield req
-                self.instructions = self.instructions - system.instructionsRead
-                print("Le quedan", self.instructions,"al proceso",self.name)
+                self.instructions -= system.instructionsRead
+                self.cronometro += 1
+                print("Tiempo proceso",self.name,"es",self.cronometro)
+                print("Le quedan", self.instructions,"instrucciones al proceso",self.name,"\n")
+                
+            if self.instructions <=0:
+                print("Se termino de ejecutar el programa")
+                yield system.ram.put(self.memory)
+                print("memoria ram",self.system.ram.level,"\n")
             
 env = simpy.Environment()
 sistema = System(env, 3)
