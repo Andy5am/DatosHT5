18fc54a7015ff7e62f2bca3924c30a60aa9b5f7f
Andy5am <38666859+Andy5am@users.noreply.github.com>
2019-03-01 13:14:42 -0600

    Se agrego el intervalo

--- DatosHT5.py
+++ DatosHT5.py
@@ -10,7 +10,12 @@ import statistics as stats
 
 random.seed(5)
 
+#listas para tiempos de inicio y fin
+listaInicio = []
 listaTiempos = []
+#lista para el promedio
+listaPromedio=[]
+
 
 #clase System, el sistema operativo.
 class System:
@@ -41,40 +46,51 @@ class Process:
         print("Memoria asignada al proceso",self.name,"\n")
         #empieza simulacion del sistema operativo.
         while self.instructions > 0:
+            #se solicita usar el cpu
             with system.cpu.request() as req:
                 yield req
+                #se restan las instrucciones
                 self.instructions -= system.instructionsRead
-                self.cronometro += 1
                 if self.instructions>0:
                     print("Le quedan", self.instructions,"instrucciones al proceso",self.name,"\n")
                 yield env.timeout(1)
-                
+                #se genera el numero random y se va a wait o ready
                 waiting = random.randint(1,2)
                 if waiting == 1:
                     print("El proceso",self.name,"esta en waiting durante 1 unidad de tiempo")
                     yield env.timeout(1)
                     print("El proceso",self.name,"esta en ready\n")
-                
+            # si ya o tiene instrucciones termina
             if self.instructions <=0:
                 print("Se termino de ejecutar el proceso",self.name,"\n")
+                self.cronometro= env.now
                 yield system.ram.put(self.memory)
                 listaTiempos.append(self.cronometro)
-            
+
+#Se creo el environment         
 env = simpy.Environment()
+#se crea el sistema operativo
 sistema = System(env, 3)
+#metodo para generar procesos
+def processGenerator(env,num):
+    for i in range(num):
+        name = i
+        memory = random.randint(1,10)
+        instructions = random.randint(1,10)
+        listaInicio.append(env.now)
+        process = Process(env,name,memory,instructions,sistema)
+        yield env.timeout(random.expovariate(1.0/5))
 
-#crea los procesos para la simulacion.
-for i in range(10):
-    name = i
-    memory = random.randint(1,10)
-    instructions = random.randint(1,10)
-    process = Process(env,name,memory,instructions,sistema)
-
-
+#Simulacion
+env.process(processGenerator(env,200))
 env.run()
-tiempo = env.now
-#mostrar cakculos y estadisticas de la simulacion.
-print("El tiempo total de la ejecucion es de",tiempo)
-print("El promedio de tiempo de ejecucion de cada proceso es de",stats.mean(listaTiempos))
-print("La desviacion estandar del tiempo de ejecucion de cada proceso es de",stats.pstdev(listaTiempos))
 
+
+tiempo = env.now
+#for para tiempos
+for a in range(len(listaInicio)):
+    listaPromedio.append(listaTiempos[a]-listaInicio[a])
+#se muestran tiempos
+print("El tiempo total de la ejecucion es de",tiempo)
+print("El promedio de tiempo de ejecucion de cada proceso es de",stats.mean(listaPromedio))
+print("La desviacion estandar del tiempo de ejecucion de cada proceso es de",stats.pstdev(listaPromedio))
